0cdd86d13f13cc25a7ef93eb90ff3ed127c71b33
anilyagiz <anill.yagiz@gmail.com>
2023-05-25 20:02:38 +0300

    -

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Gatecon</span>,
+  project: {
+    link: 'https://github.com/anilyagiz',
+  },
+  chat: {
+    link: 'https://discord.gg/wZh2HgGDZV',
+  },
+  docsRepositoryBase: 'https://github.com/anilyagiz',
+  footer: {
+    text: 'Açık Eğitim, eğitimde eşitlik sağlamak amacıyla kurulmuş kar amacı gütmeyen bir platformdur. ',
+  },
+}
+
+export default config
